2d67a64311dcd91e0909308e698f39e558db8002
Michal Koperski <mkoperpno@gmail.com>
2020-09-30 21:18:22 +0200

    add findCarId method

--- src/main/java/DAOCar.java
+++ src/main/java/DAOCar.java
@@ -1,8 +1,46 @@
+import java.sql.*;
+
 public class DAOCar {
 
     DBProperties properties = new DBProperties();
 
-    private final String addCar = "Insert Into car VALUES (?,?,?)"; //nazwa, segment, benzyna/diesel
-    private final String selectCarById = "SELECT * FROM car WHERE id = ?";
-    private final String  deleteCarById= "DELETE FROM car WHERE id =?";
+    private final String addCar = "Insert Into cars VALUES (?,?,?)"; //nazwa, segment, benzyna/diesel
+    private final String selectCarById = "SELECT * FROM cars WHERE id = ?";
+    private final String deleteCarById = "DELETE FROM cars WHERE id =?";
+    private final String findById = "SELECT * FROM cars WHERE id =?";
+
+    public void findById(int id) {
+        Connection connection = null;
+        PreparedStatement preStm = null;
+
+        try {
+            connection = DriverManager.getConnection(properties.getUrl(), properties.getUser(), properties.getPassword());
+            preStm = connection.prepareStatement(selectCarById);
+
+            preStm.setInt(1, id);
+            ResultSet resultSet = preStm.executeQuery();
+
+            while (resultSet.next()) {
+                id = resultSet.getInt("car_id");
+                String name = resultSet.getString("name");
+                String segment = resultSet.getString("segment");
+                String fuel = resultSet.getString("fuel");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                preStm.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+    }
 }
--- src/main/java/DBProperties.java
+++ src/main/java/DBProperties.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 @Setter
 public class DBProperties {
 
-    private final String url = "";
-    private final String user ="";
-    private final String password = "";
+    private final String url = "jdbc:mysql://localhost:3306/cars";
+    private final String user ="car_user";
+    private final String password = "Marik1234";
 }
